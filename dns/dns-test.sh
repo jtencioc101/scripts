5147204d6087a23200f792faae71230055e38ca2
Julio <j.tenciocoto@gmail.com>
2025-03-27 16:56:28 -0600

    adding wireserver logic

--- dns/dns-test.sh
+++ dns/dns-test.sh
@@ -1,13 +1,9 @@
+#!/bin/bash
+
 # Function to prompt user input
 prompt_user() {
     read -p "$1: " $2
 }
-
-# Check if jq is installed
-#if ! command -v jq &> /dev/null; then
-#    echo "jq could not be found. Please install it first."
-#    exit 1
-#fi
 
 # Prompt user for required data
 prompt_user "Enter the name of the VNet" VNET_NAME
@@ -15,12 +11,13 @@ prompt_user "Enter the resource group name" RESOURCE_GROUP
 prompt_user "Enter the AKS nodepool name" AKS_NODEPOOL_NAME
 
 # Step 1: Retrieve DNS server IP addresses from the VNet
-DNS_SERVER_IPS=$(az network vnet show --name "$VNET_NAME" -g "$RESOURCE_GROUP" -o json| jq -r '.dhcpOptions.dnsServers[]')
+DNS_SERVER_IPS=$(az network vnet show --name "$VNET_NAME" --resource-group "$RESOURCE_GROUP" --query "dhcpOptions.dnsServers[*]" -o tsv)
 
-# Check if we got any DNS servers
+# Check if we got any DNS servers and handle errors
 if [ -z "$DNS_SERVER_IPS" ]; then
-  echo "No DNS server IP addresses found for VNet $VNET_NAME."
-  exit 1
+    # If no DNS servers are found, set the default Azure DNS IP address
+    DNS_SERVER_IPS="168.63.129.16"
+    echo "The VNet is configured with the default Azure DNS."
 fi
 
 echo "Retrieved DNS Servers: $DNS_SERVER_IPS"
@@ -39,10 +36,8 @@ run_nslookups() {
     
     if [ $? -eq 0 ]; then
         echo "$result" >> success_output.log
-        return 0
     else
         echo "FQDN: $fqdn, DNS Server IP: $dns_server_ip, Error: $result" >> error_output.log
-        return 1
     fi
 }
 
@@ -70,22 +65,9 @@ EOF
 echo "Creating debug pod with name $DEBUG_POD_NAME..."
 kubectl apply -f $YAML_FILE
 
-# Check if the debug pod was created successfully
-if [ $? -ne 0 ]; then
-  echo "Failed to create debug pod."
-  exit 1
-fi
-
-echo "Debug pod created: $DEBUG_POD_NAME"
-
 # Wait for the debug pod to be running
 echo "Waiting for the debug pod to be running..."
 kubectl wait --for=condition=Ready pod/$DEBUG_POD_NAME
-
-if [ $? -ne 0 ]; then
-  echo "The debug pod is not ready."
-  exit 1
-fi
 
 # Run nslookup queries and log results into separate files
 echo "Running nslookup for FQDNs ${FQDNS_ARRAY[@]} against all DNS servers in VNet..."
@@ -103,14 +85,10 @@ for dns_server_ip in $DNS_SERVER_IPS; do
     done
 done
 
-# Newline before summary
-echo -e "\nSummary:"
-echo "Successful nslookup queries: $success_count"
-echo "Failed nslookup queries: $error_count"
+echo "Nslookup completed. Success: $success_count, Errors: $error_count"
 
-# Log files location
-echo "Results are logged in the following files:"
-echo "1. success_output.log (Successful nslookup queries)"
-echo "2. error_output.log (Failed nslookup queries)"
+# Clean up debug pod
+kubectl delete pod $DEBUG_POD_NAME --force 2>&1
+rm -f $YAML_FILE
 
 exit 0
